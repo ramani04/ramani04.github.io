441de90c471808bbc7ad65dee0227b81e92cf4d8
ramani04 <70248223+ramani04@users.noreply.github.com>
2020-08-26 10:47:40 +0530

    Delete blood.js

--- onlinebloodbank/blood.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const BloodbankSchema = new mongoose.Schema({
-  	sno: String,
-	Address : String,
-	blood_group : String,
-	Contact : String
-});
-module.exports = mongoose.model("Bloodbank", BloodbankSchema);
